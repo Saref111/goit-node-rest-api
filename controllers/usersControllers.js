4f23ca327c98bef2971b69f6877dbb508452ad0e
Ivan Borovyk <Ivan_Borovyk@epam.com>
2024-05-27 17:23:58 +0300

    feat: Add avatarURL to user schema and update user creation

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -2,50 +2,70 @@ import { getTokenfromReq } from "../helpers/getTokenfromReq.js";
 import usersService from "../services/usersServices.js";
 
 export const register = async (req, res) => {
-    const { email, password } = req.body;
-    const user = await usersService.findUserByEmail(email);
-    if (user) {
-        return res.status(409).json({ message: "Email in use" });
-    }
-    const newUser = await usersService.createUser(email, password);
-    return res.status(201).json({ user: { email: newUser.email, subscription: newUser.subscription } });
+  const { email, password } = req.body;
+  const user = await usersService.findUserByEmail(email);
+  if (user) {
+    return res.status(409).json({ message: "Email in use" });
+  }
+  const newUser = await usersService.createUser(email, password);
+  return res.status(201).json({
+    user: { email: newUser.email, subscription: newUser.subscription },
+  });
 };
 
 export const login = async (req, res) => {
-    const { email, password } = req.body;
-    const user = await usersService.findUserByEmail(email);
-    if (!user || !user.validPassword(password)) {
-        return res.status(401).json({ message: "Email or password is wrong" });
-    }
-    const token = await usersService.createToken(user);
-    return res.status(200).json({ token, user: { email: user.email, subscription: user.subscription } });
+  const { email, password } = req.body;
+  const user = await usersService.findUserByEmail(email);
+  if (!user || !user.validPassword(password)) {
+    return res.status(401).json({ message: "Email or password is wrong" });
+  }
+  const token = await usersService.createToken(user);
+  return res.status(200).json({
+    token,
+    user: {
+      email: user.email,
+      subscription: user.subscription,
+      avatarURL: user.avatarURL,
+    },
+  });
 };
 
 export const logout = async (req, res) => {
-    const user = await usersService.findUserById(req.user._id);
-    const token = getTokenfromReq(req);
-    if (!user) {
-        return res.status(401).json({ message: "Not authorized" });
-    }
-    await usersService.blacklistToken(token);
-    await usersService.updateToken(user._id, null);
-    return res.status(204).json();
+  const user = await usersService.findUserById(req.user._id);
+  const token = getTokenfromReq(req);
+  if (!user) {
+    return res.status(401).json({ message: "Not authorized" });
+  }
+  await usersService.blacklistToken(token);
+  await usersService.updateToken(user._id, null);
+  return res.status(204).json();
 };
 
 export const getCurrentUser = async (req, res) => {
-    const user = await usersService.findUserById(req.user._id);
-    if (!user) {
-        return res.status(401).json({ message: "Not authorized" });
-    }
-    
-    return res.status(200).json({ email: user.email, subscription: user.subscription });
+  const user = await usersService.findUserById(req.user._id);
+  if (!user) {
+    return res.status(401).json({ message: "Not authorized" });
+  }
+
+  return res
+    .status(200)
+    .json({
+      email: user.email,
+      subscription: user.subscription,
+      avararURL: user.avatarURL,
+    });
 };
 
 export const updateSubscription = async (req, res) => {
-    const user = await usersService.findUserById(req.user._id);
-    if (!user) {
-        return res.status(401).json({ message: "Not authorized" });
-    }
-    const updatedUser = await usersService.updateSubscription(user._id, req.body.subscription);
-    return res.status(200).json({ email: updatedUser.email, subscription: updatedUser.subscription });
+  const user = await usersService.findUserById(req.user._id);
+  if (!user) {
+    return res.status(401).json({ message: "Not authorized" });
+  }
+  const updatedUser = await usersService.updateSubscription(
+    user._id,
+    req.body.subscription
+  );
+  return res
+    .status(200)
+    .json({ email: updatedUser.email, subscription: updatedUser.subscription });
 };
